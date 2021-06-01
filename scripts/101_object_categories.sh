dee629abd010a323057d544270cde4bcb1e9acda
Luokun <olooook@outlook.com>
2021-06-01 10:40:57 +0800

    .

--- scripts/101_object_categories.sh
+++ scripts/101_object_categories.sh
@@ -5,16 +5,15 @@ python3 ./train.py \
     --model resnet18 \
     --classes 102 \
     --batch 64 \
-    --workers 12 \
-    --gpu 1
+    --workers 16 \
+    --gpus 0,1,2,3
 
 python3 ./train.py \
     --data data/101_ObjectCategories \
-    --checkpoints checkpoints/0 \
-    --checkpoint epoch=83_loss=1.39.pkl \
+    --checkpoints checkpoints/2 \
+    --checkpoint epoch=24_loss=2.04.pkl \
     --model resnet18 \
     --classes 102 \
     --batch 64 \
-    --workers 12 \
-    --epochs 200 \
-    --gpus 0,1
+    --workers 16 \
+    --gpus 0,1,2,3
